4b38da89096b3c148356243007fa12bb1dd53696
doffylamingo <102771222+doffylamingo@users.noreply.github.com>
2025-06-18 19:18:51 +0800

    feat: add ping slash command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,24 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Command } from "@sapphire/framework";
-import type { Message } from "discord.js";
+import type { ChatInputCommandInteraction, Message } from "discord.js";
 
 @ApplyOptions<Command.Options>({
   description: "ping pong",
 })
 export class PingCommand extends Command {
+  public override registerApplicationCommands(registry: Command.Registry) {
+    registry.registerChatInputCommand(builder =>
+      builder.setName(this.name).setDescription(this.description),
+    );
+  }
+
+  public override async chatInputRun(interaction: ChatInputCommandInteraction) {
+    const ping = Date.now() - interaction.createdTimestamp;
+    return interaction.reply({
+      content: `Pong!!! Latency is ${ping}ms.`,
+    });
+  }
+
   public override async messageRun(message: Message) {
     const ping = Date.now() - message.createdTimestamp;
     return message.reply({
